f88fc1207a0a9cc6c107c15b77daca0df52d7855
Everton Brzozowy Alves <51219903+EvertonBrzozowyAlves@users.noreply.github.com>
2023-10-15 11:27:46 -0300

    Create wezterm.lua

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'Fira Code Nerd Font'
+config.color_scheme = 'Catppuccin Mocha'
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.enable_scroll_bar = false
+config.default_prog = { '/usr/bin/nu', '-l' }
+
+return config
